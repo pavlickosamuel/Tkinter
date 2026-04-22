b0edbf19e21978377d555bc64822594092725034
pavlickosamuel <pavlickosamuel@gmail.com>
2026-04-22 07:56:39 +0200

    final

--- tkn.py
+++ tkn.py
@@ -16,19 +16,19 @@ def click():
     print (farba2)
 
     if farba == "lightblue":
-        canvas.itemconfig(obj2, fill="lightpink", outline="lightpink")
+        canvas.itemconfig(obj2, fill = "lightpink", outline = "lightpink")
     else:
-        canvas.itemconfig(obj2, fill="lightblue", outline="lightblue")
+        canvas.itemconfig(obj2, fill = "lightblue", outline = "lightblue")
     if farba2 == "peachpuff":
-        canvas.itemconfig(obj3, fill="lightyellow", outline="lightyellow")
+        canvas.itemconfig(obj3, fill = "lightyellow", outline = "lightyellow")
     else:
-        canvas.itemconfig(obj3, fill="peachpuff", outline="peachpuff")
+        canvas.itemconfig(obj3, fill = "peachpuff", outline = "peachpuff")
 
-button = tk.Button(win, text="click me", bg="hotpink", width=100, height=5, command=click)
+button = tk.Button(win, text="click me", bg="hotpink", width = 100, height=5, command=click)
 button.pack()
 
-obj1 = canvas.create_line(0, 0, 800, 800, fill="lightblue", width=5)
-obj2 = canvas.create_rectangle(100,100,700,700, fill="lightblue", outline="lightblue")
-obj3 = canvas.create_oval(200,200,600,600, fill="peachpuff", outline="peachpuff")
+obj1 = canvas.create_line(0, 0, 800, 800, fill = "lightblue", width = 5)
+obj2 = canvas.create_rectangle(100,100,700,700, fill = "lightblue", outline = "lightblue")
+obj3 = canvas.create_oval(200,200,600,600, fill = "peachpuff", outline = "peachpuff")
 
 win.mainloop()
